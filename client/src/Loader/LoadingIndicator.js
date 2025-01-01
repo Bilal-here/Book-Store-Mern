1851520837ee1800e6a25809732e23ee98af828d
Bilal <bilalahmsiddique@gmail.com>
2025-01-02 00:31:03 +0530

    Added Loading text

--- client/src/Loader/LoadingIndicator.js
+++ client/src/Loader/LoadingIndicator.js
@@ -5,6 +5,7 @@ function LoadingIndicator() {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
       <Slab color="#4f46e5" size="large" text="Loading..." textColor="" />
+      Loading..
     </div>
   );
 }
